f7cf473211535350766a08c81128ccd7715ad57f
Eli Ribble <eliribble@google.com>
2019-03-04 17:22:05 +0000

    Add test for nested large strings.

---
+++ test/output/57.py
@@ -0,0 +1,8 @@
+def outer(f):
+	def nested(g):
+		def doublenested(h):
+			return (
+				"This is going to be a really long string that is super nested inside a"
+				"bunch of definitions. Just want to make sure it gets sanely broken up"
+				"into parts."
+			)
