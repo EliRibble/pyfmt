a8a4582b92398ebd13798987bc53896bccedb76e
Eli Ribble <eliribble@google.com>
2019-03-08 04:12:58 +0000

    Add missing large string format tests.
    Oops.

---
+++ test/input/55.py
@@ -0,0 +1,4 @@
+def get_big_string():
+    return ("This is a long string. I mean, like really really long. How did I make it so long? I just kept typing. I typed a lot. Without any newlines. That may seem like a bad idea, but if you're writing a test it may be just the thing that you need. Can my formatter handle it? I guess we'll see.")
+
+print(get_big_string())
